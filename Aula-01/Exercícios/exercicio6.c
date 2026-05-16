9ae0ed1576bee55a57e354193375a50d85602344
mtthew-dev <mtthew.suppr@gmail.com>
2026-05-16 19:40:18 -0300

    Mudança de Nomes;
    Mudei os nomes, para ficar mais fácil de onde começar por qual coisa.

--- Aula-01/Exercícios/exercicio6.c
+++ Aula-01/Exercícios/exercicio6.c
@@ -2,11 +2,11 @@
 
 int main() {
 
-		/*Exercicio de Resto da Divisao.*/
+		/*Exercício de Resto da Divisão.*/
 
 	int n1, n2;
 
-	printf("Digite 2 numeros (Com espaco): ");
+	printf("Digite 2 números (Com espaço): ");
 		scanf("%d %d", &n1, &n2);
 		
 	printf("Quociente = %d\n", n1 / n2);
